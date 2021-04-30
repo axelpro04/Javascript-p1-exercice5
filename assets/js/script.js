5526960344742603d240e33a14c2ea3c96c5cb21
axelpro04 <axellefpro@gmail.com>
2021-04-30 10:00:31 +0200

    exo

---
+++ assets/js/script.js
@@ -0,0 +1,5 @@
+function touch() {
+    var firstNumber = document.getElementById('firstNumber').value;
+    var secondNumber = document.getElementById('secondNumber').value;
+    alert('Reste : ' + firstNumber % secondNumber);
+}
